6acb72b08312163e5293723bc85c904504453586
CoolShivas <shivas2710cool00@gmail.com>
2025-02-03 19:57:19 +0530

    Added the App.css file and added the pages on the App.jsx file.

--- world-atlas-using-react-19/src/App.jsx
+++ world-atlas-using-react-19/src/App.jsx
@@ -1,9 +1,17 @@
-
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import ContactPage from "./pages/ContactPage";
+import CountryPage from "./pages/CountryPage";
+import AboutPage from "./pages/AboutPage";
 
 const App = () => {
   return (
     <div>
         <h1>Hello World</h1>      
+        <HomePage></HomePage>
+        <ContactPage></ContactPage>
+        <CountryPage></CountryPage>
+        <AboutPage></AboutPage>
     </div>
   )
 }
