ff3dc44649b811da8953fe093643a46161325a8d
CoolShivas <shivas2710cool00@gmail.com>
2025-02-03 21:29:10 +0530

    Formation of two folders i.e, Layout and Ui.

---
+++ world-atlas-using-react-19/src/components/Layout/AppLayOut.jsx
@@ -0,0 +1,14 @@
+import Header from "../Ui/Header";
+import Footer from "../Ui/Footer";
+
+const AppLayOut = () => {
+  return (
+    <div>
+        <h1>Welcome to the AppLayout Page.</h1>
+        <Header></Header>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default AppLayOut;
---
+++ world-atlas-using-react-19/src/components/Ui/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+const Footer = () => {
+  return (
+    <div>
+        <h1>Welcome to the Header Page.</h1>      
+    </div>
+  )
+}
+
+export default Footer;
