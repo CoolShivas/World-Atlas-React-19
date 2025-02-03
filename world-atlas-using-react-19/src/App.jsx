aa91f4401cb8beaab5ec4392e07896966f349c01
CoolShivas <shivas2710cool00@gmail.com>
2025-02-03 20:08:30 +0530

    Implementation of Router paths in App.jsx file.

--- world-atlas-using-react-19/src/App.jsx
+++ world-atlas-using-react-19/src/App.jsx
@@ -1,18 +1,40 @@
 import "./App.css";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import CountryPage from "./pages/CountryPage";
 import AboutPage from "./pages/AboutPage";
 
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomePage></HomePage>,
+  },
+  {
+    path:"about",
+    element:<AboutPage></AboutPage>,
+  },
+  {
+    path:"country",
+    element:<CountryPage></CountryPage>,
+  },
+  {
+    path:"contact",
+    element:<ContactPage></ContactPage>,
+  },
+]);
+
 const App = () => {
   return (
-    <div>
-        <h1>Hello World</h1>      
-        <HomePage></HomePage>
-        <ContactPage></ContactPage>
-        <CountryPage></CountryPage>
-        <AboutPage></AboutPage>
-    </div>
+    // <div>
+    //     <h1>Hello World</h1>      
+    //     <HomePage></HomePage>
+    //     <ContactPage></ContactPage>
+    //     <CountryPage></CountryPage>
+    //     <AboutPage></AboutPage>
+    // </div>
+    <RouterProvider router={router}></RouterProvider>
   )
 }
 
