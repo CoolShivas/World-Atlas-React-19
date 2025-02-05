80af63967816ed24456850ef2804babe7ec70cd4
CoolShivas <shivas2710cool00@gmail.com>
2025-02-05 15:37:18 +0530

    Use of useRouteError for the Error page.

--- world-atlas-using-react-19/src/pages/ErrorPage.jsx
+++ world-atlas-using-react-19/src/pages/ErrorPage.jsx
@@ -1,9 +1,15 @@
+import { useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
+  
+  const err = useRouteError();
+  console.log(err);
+
   return (
     <center>
         <h1>Oops! Something went wrong.</h1>
+        {err && <p>{err.data}</p>}
     </center>
   )
 }
