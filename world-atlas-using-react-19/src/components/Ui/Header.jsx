bc01934e6373b58b86e7d3975d748f28bb307b59
CoolShivas <shivas2710cool00@gmail.com>
2025-02-06 12:11:39 +0530

    Formation of Header and Navbar.

--- world-atlas-using-react-19/src/components/Ui/Header.jsx
+++ world-atlas-using-react-19/src/components/Ui/Header.jsx
@@ -1,10 +1,28 @@
-
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-        <h1>Welcome to the Header Page.</h1>      
-    </div>
+    <header>
+      <div className="container">
+        <div className="grid navbar-grid">
+          {/* Starting of Logo Heading */}
+          <div className="Logo">
+            <NavLink to="/"><h1>World Atlas</h1></NavLink>
+          </div>
+          {/* Ending of Logo Heading */}
+          {/* Starting of Navbar */}
+          <nav>
+            <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/country">Country</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+          </nav>
+          {/* Ending of Navbar */}
+        </div>
+      </div>
+    </header>
   )
 }
 
