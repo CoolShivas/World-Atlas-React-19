cae9ec9a3a450cfdb32d5f7dee87f075a5fbd7ff
CoolShivas <shivas2710cool00@gmail.com>
2025-02-07 12:54:21 +0530

    About Page card lists with hard coded data.

--- world-atlas-using-react-19/src/pages/AboutPage.jsx
+++ world-atlas-using-react-19/src/pages/AboutPage.jsx
@@ -12,6 +12,7 @@ const AboutPage = () => {
       {/* Ending of About Page Heading */}
       {/* Starting of Cards List Structure. */}
       <div className="gradient-cards">
+         {/* Starting of First Card Details */}
         <div className="card">
           <div className="container-card bg-blue-box">
             <p className="card-title">India</p>
@@ -29,6 +30,26 @@ const AboutPage = () => {
             </p>
           </div>
         </div>
+        {/* Ending of First Card Details */}
+        {/* Starting of Second Card Details */}
+        <div className="card">
+          <div className="container-card bg-blue-box">
+            <p className="card-title">China</p>
+            <p>
+              <span className="card-description">Capital:</span>
+              Bejing
+            </p>
+            <p>
+              <span className="card-description">Population:</span>
+              13546465465
+            </p>
+            <p>
+              <span className="card-description">Interesting Facts:</span>
+              They also Love India
+            </p>
+          </div>
+        </div>
+        {/* Ending of Second Card Details */}
       </div>
       {/* Ending of Cards List Structure. */}
     </section>
