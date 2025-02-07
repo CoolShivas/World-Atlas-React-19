0fad5259a30321e68382bc7873ff019e6f1354b4
CoolShivas <shivas2710cool00@gmail.com>
2025-02-07 12:50:36 +0530

    Formation of About Page card structure.

--- world-atlas-using-react-19/src/pages/AboutPage.jsx
+++ world-atlas-using-react-19/src/pages/AboutPage.jsx
@@ -3,12 +3,34 @@
 const AboutPage = () => {
   return (
     <section className="section-about container">
+      {/* Starting of About Page Heading */}
       <h2 className="container-title">
         Here are the Interesting Facts
         <br />
         we're proud of 
       </h2>
-      
+      {/* Ending of About Page Heading */}
+      {/* Starting of Cards List Structure. */}
+      <div className="gradient-cards">
+        <div className="card">
+          <div className="container-card bg-blue-box">
+            <p className="card-title">India</p>
+            <p>
+              <span className="card-description">Capital:</span>
+              Delhi
+            </p>
+            <p>
+              <span className="card-description">Population:</span>
+              14546465465
+            </p>
+            <p>
+              <span className="card-description">Interesting Facts:</span>
+              We Love India
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* Ending of Cards List Structure. */}
     </section>
   )
 }
