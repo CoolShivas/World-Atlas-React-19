ae781119eb16532356cee474258f95b2e1a4f4b5
CoolShivas <shivas2710cool00@gmail.com>
2025-02-07 17:14:49 +0530

    ContactPage structure is completed.

--- world-atlas-using-react-19/src/pages/ContactPage.jsx
+++ world-atlas-using-react-19/src/pages/ContactPage.jsx
@@ -2,9 +2,22 @@
 
 const ContactPage = () => {
   return (
-    <div>
-        <h1>Welcome to the Contact Page.</h1>
-    </div>
+    <section className="section-contact">
+      <h2 className="container-title">Contact Us</h2>
+      <div className="contact-wrapper container">
+        <form action="#">
+          
+          <input type="text" className="form-control" placeholder="Enter your name" name="username" autoComplete="false" required/>
+
+          <input type="email" className="form-control" placeholder="Enter your email" name="username" autoComplete="false" required/>
+
+          <textarea className="form-control" rows="10" placeholder="Enter your message" name="message" autoComplete="false"></textarea>
+
+          <button type="submit" value="send">Send</button>
+
+        </form>
+      </div>
+    </section>
   )
 }
 
