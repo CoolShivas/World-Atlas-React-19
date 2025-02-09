9b09afbe167ebef60ec646a75f72ee2294b78a59
CoolShivas <shivas2710cool00@gmail.com>
2025-02-09 13:43:36 +0530

    Previously, getting the footer api data but instead of icons getting their names only now resolved.

--- world-atlas-using-react-19/src/components/Ui/Footer.jsx
+++ world-atlas-using-react-19/src/components/Ui/Footer.jsx
@@ -1,6 +1,12 @@
 import footerData from "../../api/footerData.json";
+import {MdPlace} from "react-icons/md";
 
 const Footer = () => {
+
+  const footerIcons = {
+    MdPlace : <MdPlace/>,
+  };
+
   return (
     <footer className="footer-section">
       <div className="container grid grid-three-cols">
@@ -8,7 +14,7 @@ const Footer = () => {
           const {icon, title, details} = currData;
           return (
             <div className="footer-contact" key={index}>
-              <div className="icon">{icon}</div>
+              <div className="icon">{footerIcons.MdPlace}</div>
                 <div className="footer-contact-text">
                   <p>{title}</p>
                   <p>{details}</p>
