ff38bf9d18db33a4868477348220dead16caad97
CoolShivas <shivas2710cool00@gmail.com>
2025-02-09 13:30:21 +0530

    Getting the api data from the api folder to FooterPage with the help of map method and destructure it also.

--- world-atlas-using-react-19/src/components/Ui/Footer.jsx
+++ world-atlas-using-react-19/src/components/Ui/Footer.jsx
@@ -1,20 +1,24 @@
-
+import footerData from "../../api/footerData.json";
 
 const Footer = () => {
   return (
     <footer className="footer-section">
       <div className="container grid grid-three-cols">
-        <div className="footer-contact">
-          <div className="icon">
-            <div className="footer-contact-text">
-              <p>Title</p>
-              <p>Details</p>
+        {footerData.map((currData, index) => {
+          const {icon, title, details} = currData;
+          return (
+            <div className="footer-contact" key={index}>
+              <div className="icon">{icon}</div>
+                <div className="footer-contact-text">
+                  <p>{title}</p>
+                  <p>{details}</p>
+                </div>
             </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
