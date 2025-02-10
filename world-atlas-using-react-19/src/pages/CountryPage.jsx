f32faab7fd848fa5272a07d5868471dcc79e0b6b
CoolShivas <shivas2710cool00@gmail.com>
2025-02-10 16:41:50 +0530

    Mapping the data of postApi using useState in the CountryPage.

--- world-atlas-using-react-19/src/pages/CountryPage.jsx
+++ world-atlas-using-react-19/src/pages/CountryPage.jsx
@@ -1,15 +1,18 @@
 import { Loader } from "../components/Ui/Loader";
 import { getCountriesData } from "../api/postApi";
-import { useEffect, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 
 const CountryPage = () => {
   // // useTransition is a React Hook that lets you render a part of the UI in the background.
   const [isPending, startTransition] = useTransition();
+  const [countries, setCountries] = useState([]);
 
   useEffect(() => {
     startTransition(async () => {
       const response = await getCountriesData();
-      console.log(response);
+      // console.log(response);
+      setCountries(response.data);
+      console.log(response.data);
     });
   }, []);
 
@@ -24,6 +27,11 @@ const CountryPage = () => {
   return (
     <div>
       <h1>Welcome to the Country Page.</h1>
+      {countries.map((curCounty, index) => {
+        return <center>
+          <div key={index} className="brightColor">{curCounty.name.common}</div>
+        </center>;
+      })}
     </div>
   );
 };
