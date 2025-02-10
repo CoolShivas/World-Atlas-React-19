54c2fa09d3f9d1dc47bb60239ea1b25dc9870040
CoolShivas <shivas2710cool00@gmail.com>
2025-02-10 11:48:38 +0530

    Getting the data from the postApi file from the server. And, also used the useTransition and useEffect hooks.

--- world-atlas-using-react-19/src/pages/CountryPage.jsx
+++ world-atlas-using-react-19/src/pages/CountryPage.jsx
@@ -1,11 +1,30 @@
-
+import { getCountriesData } from "../api/postApi";
+import { useEffect, useTransition } from "react";
 
 const CountryPage = () => {
+  // // useTransition is a React Hook that lets you render a part of the UI in the background.
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(async () => {
+      const response = await getCountriesData();
+      console.log(response);
+    });
+  }, []);
+
+  if (isPending) {
+    return (
+      <center>
+        <h1> Loading... </h1>
+      </center>
+    );
+  }
+
   return (
     <div>
-        <h1>Welcome to the Country Page.</h1>
+      <h1>Welcome to the Country Page.</h1>
     </div>
-  )
-}
+  );
+};
 
 export default CountryPage;
