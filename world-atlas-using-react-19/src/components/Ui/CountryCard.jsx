ba798fe2cc8e0c9b47d449e02b2abea3253859b5
CoolShivas <shivas2710cool00@gmail.com>
2025-02-11 14:03:09 +0530

    Use of Ternary Operator and Slice Method for making the Country Name short.

--- world-atlas-using-react-19/src/components/Ui/CountryCard.jsx
+++ world-atlas-using-react-19/src/components/Ui/CountryCard.jsx
@@ -6,7 +6,10 @@ const CountryCard = ({ conTree }) => {
       <div className="container-card bg-white-box">
         <img src={flags.svg} alt="flags.alt" />
         <div className="countryInfo">
-          <p className="card-title">{name.common}</p>
+          <p className="card-title">
+            {name.common.length > 10 ? name.common.slice(0, 10) + "..." : name.common}
+            {/* Use of ternary operator for the long name of a country making under the range by using slice method and concatenate  */}
+          </p>
           <p>
             <span className="card-description">Population : </span>
             {population.toLocaleString()}
