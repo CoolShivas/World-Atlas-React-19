cbbc92f3bb3e6012cf69c3c1535b74850e52831d
CoolShivas <shivas2710cool00@gmail.com>
2025-02-11 13:28:19 +0530

    Given the structure to the CountryCard and destructuring the given attribute and pass that data.

--- world-atlas-using-react-19/src/components/Ui/CountryCard.jsx
+++ world-atlas-using-react-19/src/components/Ui/CountryCard.jsx
@@ -1,9 +1,28 @@
+const CountryCard = ({ conTree }) => {
+  const { flags, name, population, region, capital } = conTree;
 
-
-const CountryCard = ({conTree}) => {
   return (
-    <li>{conTree.name.common}</li>
-  )
-}
+    <li className="country-card card">
+      <div className="container-card bg-white-box">
+        <img src={flags.svg} alt="flags.alt" />
+        <div className="countryInfo">
+          <p className="card-title">{name.common}</p>
+          <p>
+            <span className="card-description">Population : </span>
+            {population}
+          </p>
+          <p>
+            <span className="card-description">Region : </span>
+            {region}
+          </p>
+          <p>
+            <span className="card-description">Capital : </span>
+            {capital}
+          </p>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default CountryCard;
