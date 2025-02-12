800d614558a0e7b2539001855fbb6720b9619958
CoolShivas <shivas2710cool00@gmail.com>
2025-02-12 15:03:26 +0530

    Without mapping the data of postApi using useState in the CountryDetailsPage. Because data is already getting in key and pair form.

--- world-atlas-using-react-19/src/pages/CountryDetailsPage.jsx
+++ world-atlas-using-react-19/src/pages/CountryDetailsPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useTransition } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { useParams } from "react-router-dom";
 import { getCountriesIndividualData } from "../api/postApi";
 import { Loader } from "../components/Ui/Loader";
@@ -10,12 +10,15 @@ const CountryDetailsPage = () => {
   console.log(params);
 
   const [isPending, startTransition] = useTransition();
+  const [individualCountry, setIndividualCountry] = useState([]);
 
   useEffect(() => {
     startTransition(async ()=>{
       const res = await getCountriesIndividualData(params.id);
       // // Passing the argument to get that particular id data from the postApi file with the help of params;
       console.log(res); // Here, we are getting the data from the api;
+      console.log(res.data[0].capital);
+      setIndividualCountry(res.data[0]);
     });
   },[]);
 
@@ -30,6 +33,7 @@ const CountryDetailsPage = () => {
     <center>
         <h1>Welcom to the Country Details Page.</h1>
         <h2>How can I help you dear ?</h2>
+        <h1>{individualCountry.capital}</h1>
     </center>
   )
 }
