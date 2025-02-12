972ca461abfa620d89c0666eaae671365af2d824
CoolShivas <shivas2710cool00@gmail.com>
2025-02-12 12:50:18 +0530

    Getting the data from the postApi file from the server. And, also used the useTransition and useEffect hooks.

--- world-atlas-using-react-19/src/pages/CountryDetailsPage.jsx
+++ world-atlas-using-react-19/src/pages/CountryDetailsPage.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useTransition } from "react";
 import { useParams } from "react-router-dom";
+import { getCountriesIndividualData } from "../api/postApi";
+import { Loader } from "../components/Ui/Loader";
 
 
 const CountryDetailsPage = () => {
 
   const params = useParams();
   console.log(params);
-  
+
+  const [isPending, startTransition] = useTransition();
+
+  useEffect(() => {
+    startTransition(async ()=>{
+      const res = await getCountriesIndividualData();
+      console.log(res);
+    });
+  },[]);
+
+
+  if(isPending){
+    return (<center>
+      <Loader></Loader>
+    </center>)
+  };
+
   return (
     <center>
         <h1>Welcom to the Country Details Page.</h1>
